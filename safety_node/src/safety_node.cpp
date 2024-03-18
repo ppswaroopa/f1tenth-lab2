cbb13be653b6ff13d5531ba3e1c5d902ec17ff38
Pranava Swaroopa <pranavaswaroopaa2000@gmail.com>
2024-03-18 15:14:09 -0700

    first commit

--- safety_node/src/safety_node.cpp
+++ safety_node/src/safety_node.cpp
@@ -24,6 +24,9 @@ public:
 
         /// TODO: create ROS subscribers and publishers
         
+        laser_subscription_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan", 1, std::bind(&Safety::scan_callback, this, std::placeholders::_1));
+        car_odom_subscription_ = this->create_subscription<nav_msgs::msg::Odometry>("ego_racecar/odom", 1, std::bind(&Safety::drive_callback, this, std::placeholders::_1));
+        ackermann_publisher_ = this->create_publisher<ackermann_msgs::msg::AckermannDriveStamped>("drive",1);
     }
 
 private:
@@ -38,11 +41,28 @@ private:
     void scan_callback(const sensor_msgs::msg::LaserScan::ConstSharedPtr scan_msg) 
     {
         /// TODO: calculate TTC
-
+        auto ranges = scan_msg->ranges;
+        float incr_ang = scan_msg->angle_increment;
+        // float min_ang = scan_msg->angle_min;
+        // float max_ang = scan_msg
+        int t = 1;
+        for (int i = 0; i < ranges.size(); i++)
+        {
+            ranges[i] = ranges[i] * ( t * scan_msg->angle_increment );
+            if (t<=1080)
+            {
+                t++;
+            }
+            else
+                break;
+        }
+        
         /// TODO: publish drive/brake message
     }
 
-
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_subscription_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr car_odom_subscription_;
+    rclcpp::Publisher<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr ackermann_publisher_;
 
 };
 int main(int argc, char ** argv) {
